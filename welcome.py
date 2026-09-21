6a6ed5f08421b144a5616f1b6dc03a21d79c700a
Berto-Mauro <bertotaban6@gmail.com>
2026-09-21 14:11:57 +0300

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,29 @@
+def welcome(name):
+    return "Hello, " + name + "! Welcome to PLP."
+
+
+print(welcome("Amina"))
+print(welcome("Brian"))
+print(welcome("Fatuma"))
+
+
+
+#What this does
+
+#This function:
+
+#def welcome(name):
+
+#creates a function called welcome.
+
+#The line:
+
+#return "Hello, " + name + "! Welcome to PLP."
+
+#creates and returns the greeting.
+
+#Then these three lines use the function:
+
+#print(welcome("Amina"))
+#print(welcome("Brian"))
+#print(welcome("Fatuma"))
